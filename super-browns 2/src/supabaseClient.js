3dcb7ac5b968140bf79523359c86d8de2dfb0c23
andrewzoss <andrewzoss@gmail.com>
2026-06-08 18:31:35 -0600

    Add files via upload

--- super-browns 2/src/supabaseClient.js
+++ super-browns 2/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wxizsssdougtfudgylzpz.supabase.co'
+const supabaseUrl = 'https://wxizssdougtfudgylzpz.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4aXpzc2RvdWd0ZnVkZ3lsenB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NTU0NTcsImV4cCI6MjA5NjUzMTQ1N30.jbc-SxmlxgpezX6jZ_JSDMyAu2mv5i6HrWmdIvE-xQ0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
